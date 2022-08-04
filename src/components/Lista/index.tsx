16662f37ff71e0a58426188a87805e51b6ab6f2b
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2022-08-04 03:33:43 -0300

    Aula 2

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import style from './Lista.module.scss'
+
+function Lista() {
+    const tarefas = [
+        {
+            tarefa: 'React',
+            tempo: '01:00:00'
+        },
+        {
+            tarefa: 'Javascrip',
+            tempo: '01:30:00'
+        }
+    ]
+    console.log('oi')
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia</h2>
+            <ul>
+                { tarefas.map( (item, index) => (
+                    <li key={index} className={style.item}>
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                )) }   
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista
